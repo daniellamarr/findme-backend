6a5e44de05added776e611f8ed8e34c601a47996
timzprof <timilehin.olumofin@gmail.com>
2019-12-26 09:39:13 +0100

    Implemented Event filtering

--- functions/src/middleware/gcloudStorage.js
+++ functions/src/middleware/gcloudStorage.js
@@ -1,86 +1,87 @@
-const path = require('path');
-const Busboy = require('busboy');
-const uuid = require('uuid4');
-const { Storage } = require('@google-cloud/storage');
-const config = require('../../config/config.json');
+const path = require("path");
+const Busboy = require("busboy");
+const uuid = require("uuid4");
+const {Storage} = require("@google-cloud/storage");
+const config = require("../../config/config.json");
 
 const storage = new Storage({
-  projectId: config.projectId,
-  keyFilename: path.resolve(__dirname, '../../config/serviceAccountKey.json'),
+	projectId: config.projectId,
+	keyFilename: path.resolve(__dirname, "../../config/serviceAccountKey.json")
 });
 
 const bucket = storage.bucket(config.storageBucket);
 
-const deleteFile = (filename) => new Promise((resolve, reject) => {
-  const file = bucket.file(filename);
-  file.delete((err, apiRes) => {
-    if (err) {
-      reject(err);
-    }
-    resolve(apiRes);
-  });
-});
+const deleteFile = filename =>
+	new Promise((resolve, reject) => {
+		const file = bucket.file(filename);
+		file.delete((err, apiRes) => {
+			if (err) {
+				reject(err);
+			}
+			resolve(apiRes);
+		});
+	});
 
 const fileUpload = (req, res, next) => {
-  const busboy = new Busboy({
-    headers: req.headers,
-    limits: {
-      fileSize: 10 * 1024 * 1024,
-    },
-  });
+	const busboy = new Busboy({
+		headers: req.headers,
+		limits: {
+			fileSize: 10 * 1024 * 1024
+		}
+	});
 
-  const fields = {};
-  let fileWrite;
+	const fields = {};
+	let fileWrite;
 
-  busboy.on('field', (key, value) => {
-    fields[key] = value;
-  });
+	busboy.on("field", (key, value) => {
+		fields[key] = value;
+	});
 
-  busboy.on('file', (fieldname, file, filename, encoding, mimetype) => {
-    if (!filename) {
-      req.body = fields;
-      return next();
-    }
-    const imageFile = bucket.file(filename);
-    console.log(`Handling file upload field ${fieldname}: ${filename}`);
+	busboy.on("file", (fieldname, file, filename, encoding, mimetype) => {
+		if (!filename) {
+			req.body = fields;
+			return next();
+		}
+		const extension = filename.split(".")[1];
+		const uniqueFilename = `${uuid()}.${extension}`;
+		const imageFile = bucket.file(uniqueFilename);
+		console.log(`Handling file upload field ${fieldname}: ${uniqueFilename}`);
 
-    fileWrite = new Promise((resolve, reject) => {
-      file
-        .pipe(imageFile.createWriteStream())
-        .on('error', reject)
-        .on('finish', () => {
-          imageFile.makePublic((err, apiRes) => {
+		fileWrite = new Promise((resolve, reject) => {
+			file
+				.pipe(imageFile.createWriteStream())
+				.on("error", reject)
+				.on("finish", () => {
+					imageFile.makePublic((err, apiRes) => {
             if (err) {
               reject(err);
             }
-            const extension = filename.split('.')[1];
-            const uniqueFilename = uuid();
             return resolve(
-              `https://storage.googleapis.com/${config.storageBucket}/${uniqueFilename}.${extension}`,
+              `https://storage.googleapis.com/${config.storageBucket}/${uniqueFilename}`
             );
           });
-        });
-    });
-    return true;
-  });
+				});
+		});
+		return true;
+	});
 
-  busboy.on('finish', () => {
-    if (fileWrite) {
-      fileWrite
-        .then((url) => {
-          req.body = fields;
-          req.file = url;
-          return next();
-        })
-        .catch(next);
-    } else {
-      req.body = fields;
-      return next();
-    }
-    return true;
-  });
+	busboy.on("finish", () => {
+		if (fileWrite) {
+			fileWrite
+				.then(url => {
+					req.body = fields;
+					req.file = url;
+					return next();
+				})
+				.catch(next);
+		} else {
+			req.body = fields;
+			return next();
+		}
+		return true;
+	});
 
-  busboy.end(req.rawBody);
+	busboy.end(req.rawBody);
 };
 
-module.exports = { fileUpload, deleteFile };
+module.exports = {fileUpload, deleteFile};
--- functions/src/routes/experiences/getExperiences.js
+++ functions/src/routes/experiences/getExperiences.js
@@ -3,21 +3,37 @@ const admin = require("firebase-admin");
 
 const db = admin.firestore();
 
-const getEvents = (req, res) => {
+const getExperiences = (req, res) => {
+	const now = new Date().getTime();
+	const eventFilter = {
+		all: experience => {
+			return experience;
+		},
+		happening: experience => {
+			return experience.startDate < now && experience.endDate > now;
+		},
+		upcoming: experience => {
+			return experience.startDate > now;
+		}
+	};
 	return db
 		.collection("experiences")
+		.where("type", "==", req.query.type)
 		.get()
 		.then(snapshot => {
-			const data = snapshot.docs.map(doc => {
+			let data = snapshot.docs.map(doc => {
 				return {...doc.data(), id: doc.id};
 			});
+			if (req.query.type === "event") {
+				data = data.filter(eventFilter[req.query.filter]);
+			}
 			return res.status(200).send({
 				success: true,
 				message: "Experiences Retrieved",
-                data
+				data
 			});
 		})
-		.catch((error) =>
+		.catch(error =>
 			res.status(500).send({
 				success: false,
 				message: error.message
@@ -25,4 +41,4 @@ const getEvents = (req, res) => {
 		);
 };
 
-module.exports = getEvents;
+module.exports = getExperiences;
