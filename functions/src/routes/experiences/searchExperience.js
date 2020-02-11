5f6aa1fa2595b6b697f480a7ee1f7d1e1577097b
timzprof <timilehin.olumofin@gmail.com>
2020-02-11 22:36:48 +0100

    Modified Search Experience Endpoint

--- functions/src/routes/experiences/searchExperience.js
+++ functions/src/routes/experiences/searchExperience.js
@@ -4,33 +4,42 @@ const admin = require("firebase-admin");
 const db = admin.firestore();
 
 const searchExperience = (req, res) => {
-    let experienceRef = db.collection("experiences");
+	let experienceRef = db.collection("experiences");
+	const {keywords, ...fieldFilter} = req.query;
 
-    // Object.keys(req.query).forEach(key => {});
-    for(const key in req.query){
-        const queryString = key.includes("attendees") ? "array-contains" : "==";
-        experienceRef = experienceRef.where(key, queryString, req.query[key]);
-    }
-    
-    return experienceRef
-        .get()
-        .then(snapshot => {
-            const data = snapshot.docs.map(snap => {
-                return {...snap.data(), id: snap.id};
-            });
-            return res.status(200).send({
-                success: true,
-                message: "Search Complete",
-                data
-            });
-        })
-        .catch(error => {
-            return res.status(500).json({
-                success: false,
-                message: error.message,
-                stack: error.stack
-            });
-        });
+	Object.keys(fieldFilter).forEach(key => {
+		const queryString = key.includes("attendees") ? "array-contains" : "==";
+		experienceRef = experienceRef.where(key, queryString, req.query[key]);
+	});
+
+	return experienceRef
+		.get()
+		.then(snapshot => {
+			let data = snapshot.docs.map(snap => {
+				return {...snap.data(), id: snap.id};
+			});
+			if (keywords) {
+				data = data.filter(experienceObj => {
+					return Object.values(experienceObj)
+                        .join(" ")
+                        .toLowerCase()
+						.includes(keywords.toLowerCase());
+				});
+			}
+
+			return res.status(200).send({
+				success: true,
+				message: "Search Complete",
+				data
+			});
+		})
+		.catch(error => {
+			return res.status(500).json({
+				success: false,
+				message: error.message,
+				stack: error.stack
+			});
+		});
 };
 
 module.exports = searchExperience;
