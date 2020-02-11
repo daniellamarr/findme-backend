9017b1f601e70e23abc119b54cdd5e99fa82af30
timzprof <timilehin.olumofin@gmail.com>
2020-02-11 07:54:30 +0100

    Implemented User can Leave Experience

--- functions/src/routes/experiences/index.js
+++ functions/src/routes/experiences/index.js
@@ -6,6 +6,7 @@ const deleteExperience = require("./deleteExperience");
 const updateExperience = require("./updateExperience");
 const attendExperience = require("./attendExperience");
 const getAttendees = require("./getAttendees");
+const leaveExperience = require('./leaveExperience');
 
 const experienceRouter = Router();
 
@@ -14,6 +15,8 @@ experienceRouter.get("/", verifyToken, getExperiences);
 experienceRouter.post("/", verifyToken, fileUpload, createExperience);
 
 experienceRouter.put("/attend", verifyToken, attendExperience);
+
+experienceRouter.put('/leave', verifyToken, leaveExperience);
 
 experienceRouter.put("/:id", verifyToken, fileUpload, updateExperience);
 
---
+++ functions/src/routes/experiences/leaveExperience.js
@@ -0,0 +1,46 @@
+/* eslint-disable promise/no-nesting */
+const admin = require("firebase-admin");
+
+const db = admin.firestore();
+
+const leaveExperience = (req, res) => {
+    const {id: userId} = req.payload;
+	const {experienceId} = req.body;
+
+	const experienceRef = db.collection("experiences").doc(experienceId);
+
+	return experienceRef
+		.get()
+		.then(docRef => {
+			if (!docRef.exists) {
+				return res.status(404).json({
+					success: false,
+					message: "Experience not found"
+				});
+			}
+			const oldExperienceData = {...docRef.data()};
+			oldExperienceData.attendees = oldExperienceData.attendees.filter(attendeeId => {
+                return attendeeId !== userId;
+            });
+
+			const experienceData = {
+				...oldExperienceData,
+				updated: new Date().getTime()
+			};
+			return experienceRef.update(experienceData).then(() => {
+				return res.status(200).send({
+					success: true,
+					message: "Thank you for attending the experience",
+					data: experienceData
+				});
+			});
+		})
+		.catch(error => {
+			return res.status(500).send({
+				success: false,
+				message: error.message
+			});
+		});
+};
+
+module.exports = leaveExperience;
